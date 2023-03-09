969db594343a014a62f6aa49f956a71b0f89c884
longg2504 <longg2504@gmail.com>
2023-03-09 13:58:18 +0700

    upload

--- Module_1/Exercise/Javascript/Operator_Exer/active.js
+++ Module_1/Exercise/Javascript/Operator_Exer/active.js
@@ -8,4 +8,6 @@ let width = parseInt(inputWidth)
 let height = parseInt(inputHeight)
 
 let area = width * height
+document.writeln("Enter width: " + width)
+document.writeln("Enter height: " + height)
 document.write("The area is: " + area)
