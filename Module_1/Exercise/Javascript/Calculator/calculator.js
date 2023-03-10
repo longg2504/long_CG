300123f35ef312f4db10ab945787aa806e3946c1
longg2504 <longg2504@gmail.com>
2023-03-10 11:32:11 +0700

    upload baitap calculator

---
+++ Module_1/Exercise/Javascript/Calculator/calculator.js
@@ -0,0 +1,29 @@
+function Add() {
+    let a = document.getElementById('numb1').value
+let b = document.getElementById('numb2').value
+    let sum = 0 
+    sum = Number(a) + Number (b)
+    document.getElementById('Result').innerHTML = ('Result is :' + sum)
+}
+
+function Sub() {
+    let a = document.getElementById('numb1').value
+let b = document.getElementById('numb2').value
+    let sub = 0 
+    sub = Number(a) - Number(b)
+    document.getElementById('Result').innerHTML = ('Result is :' + sub)
+}
+
+function Mul() {
+    let a = document.getElementById('numb1').value
+    let b = document.getElementById('numb2').value
+    let mul = Number(a) * Number(b)
+    document.getElementById('Result').innerHTML = ('Result is :' + mul)
+}
+
+function Div() {
+    let a = document.getElementById('numb1').value
+    let b = document.getElementById('numb2').value
+    let div = Number(a) / Number(b)
+    document.getElementById('Result').innerHTML = ('Result is :' + div)
+} 
