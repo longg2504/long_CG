875511534686198f3ee5a15276d991b6db3a4bdc
longg2504 <longg2504@gmail.com>
2023-03-21 10:18:04 +0700

    upload

---
+++ Module_1/ExerciseLoop/methodInArray/script2.js
@@ -0,0 +1,14 @@
+console.log("bài 2");
+var num = window.prompt();
+var str = num.toString();
+var result = [str[0]];
+
+for (var x = 1; x < str.length; x++) {
+  if (str[x - 1] % 2 === 0 && str[x] % 2 === 0) {
+    result.push("-", str[x]);
+  } else {
+    result.push(str[x]);
+  }
+}
+console.log(result.join(""));
+console.log("---------------------------------------------------------");
